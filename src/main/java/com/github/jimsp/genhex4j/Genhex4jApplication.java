cc88f36be2995a02c20be5ed324dc5fcb658fb10
alexandre.lima <alexandre.msl@gmail.com>
2024-11-24 18:31:43 -0300

    add .env

--- src/main/java/com/github/jimsp/genhex4j/Genhex4jApplication.java
+++ src/main/java/com/github/jimsp/genhex4j/Genhex4jApplication.java
@@ -3,10 +3,16 @@ package com.github.jimsp.genhex4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 public class Genhex4jApplication {
 
 	public static void main(String[] args) {
+
+		final Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+		dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
+
 		SpringApplication.run(Genhex4jApplication.class, args);
 	}
 
--- src/test/java/com/github/jimsp/genhex4j/Genhex4jApplicationTests.java
+++ src/test/java/com/github/jimsp/genhex4j/Genhex4jApplicationTests.java
@@ -3,11 +3,15 @@ package com.github.jimsp.genhex4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = { "SPRING_APPLICATION_NAME=genhex4j",
+		"SPRING_AI_OPENAI_API_KEY=0",
+		"SPRING_AI_OPENAI_BASE_URL=https://api.groq.com/openai",
+		"SPRING_AI_OPENAI_CHAT_OPTIONS_MODEL=llama-3.2-90b-text-preview",
+		"SPRING_AI_OPENAI_CHAT_OPTIONS_TEMPERATURE=0.7", "SERVER_SSL_KEY_STORE=classpath:keystore.jks",
+		"SERVER_SSL_KEY_STORE_PASSWORD=0", "SERVER_SSL_KEY_STORE_TYPE=JKS", "SERVER_SSL_KEY_ALIAS=backend-ssl" })
 class Genhex4jApplicationTests {
-
+	
 	@Test
 	void contextLoads() {
 	}
-
 }
